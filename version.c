1e8f0a599f4e41c7e75057c56543cdaae2a12001
Vincent Falardeau <vincent.falardeau@nuance.com>
2022-07-12 15:19:40 +0000

    .gitignore is now working

--- version.c
+++
@@ -1,6 +0,0 @@
-#include "version.h"
-const char *janus_build_git_sha = "0466c582762123a0ebc07811235d2ec44244801e";
-const char *janus_build_git_time = "Wed Jul  6 17:36:56 UTC 2022";
-int janus_version = 114;
-const char *janus_version_string = "0.11.4";
-const char *libnice_version_string = "0.1.19.1";
